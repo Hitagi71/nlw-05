5b2b114d9195cfe7f84859707b247ffb3515cf01
Hitagi71 <andre.gomes7110@gmail.com>
2021-04-21 14:49:47 -0300

    Implentação do resto das entidades

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express"
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
 
 /**
  * Tipos de parametros
@@ -15,7 +17,16 @@ import { SettingsController } from "./controllers/SettingsController";
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 routes.post("/settings", settingsController.create);
+
+routes.post("/users", usersController.create);
+
+routes.post("/message", messagesController.create)
+routes.get("/message/:id", messagesController.showByUser)
+
+
 
 export { routes };
